85b4a6a7b99fc8427c4a7d81f41c001a1cb469b7
Emil <emil.jonsson2@yh.nackademin.se>
2020-01-20 14:25:33 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        print("hej");
+    }
+
+    // printa ut i konsolen med ett kortare kommado
+    public static void print(String printthis) {
+        System.out.println(printthis);
+    }
+}
