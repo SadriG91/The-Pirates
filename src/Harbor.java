29a77d80cbc2c7498d3f7263494a109514cbc8b6
Dmitry Kosov <dmitry.kosov@yh.nackademin.se>
2020-01-23 11:01:57 +0100

    updated pathing

--- src/Harbor.java
+++ src/Harbor.java
@@ -1,2 +1,13 @@
 public class Harbor {
+    private String name;
+    private int storage;
+
+    Harbor(String _name, int _storage){
+        this.name = _name;
+        this.storage = _storage;
+    }
+
+    public void setStorage(int weight){
+        
+    }
 }
--- src/Route.java
+++ src/Route.java
@@ -1,2 +1,14 @@
 public class Route {
-}
+    static int getIndex(int path[][], int x, int y){
+        int i;
+        int[] array = {x, y};
+        for(i=0; i<path.length; i++){
+            if(path[i]==array){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+    
+} 
