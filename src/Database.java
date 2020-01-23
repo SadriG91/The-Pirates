1d7afcddf3645920c5a0faa486ccb211e3f30dc5
Sadri <sadrigargouri91@gmail.com>
2020-01-23 10:23:29 +0100

    updates

--- src/Database.java
+++ src/Database.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.sql.*;
+import java.util.Formatter;
 import java.util.Objects;
 
 public class Database {
@@ -15,7 +16,6 @@ public class Database {
 public static void loginPassword() {
 		JPasswordField passwordField = new JPasswordField(10);
 		passwordField.setEchoChar('*');
-		passwordField.getCursor();
 		JPanel myPanel = new JPanel();
 		myPanel.add(new JLabel("Password: "));
 		myPanel.add(passwordField);
@@ -47,10 +47,7 @@ public static Connection createConnection() {
 			{
 				try {
 					Statement myStatement = Objects.requireNonNull(createConnection()).createStatement();
-					ResultSet myRs = myStatement.executeQuery(query);
-					while (myRs.next()) {
-						System.out.println(myRs.getString("name"));
-					}
+					ResultSet(myStatement, query);
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -58,13 +55,41 @@ public static Connection createConnection() {
 
 		}
 
+	public static void getShipInfo(String myShip) {
+		{
+			try {
+				Statement myStatement = Objects.requireNonNull(createConnection()).createStatement();
+				String myString = "Select * from simulator.myships where name ="+myShip;
+				ResultSet(myStatement, myString);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private static void ResultSet(Statement myStatement, String myString) throws SQLException {
+		ResultSet myRs = myStatement.executeQuery(myString);
+		ResultSetMetaData rsmd = myRs.getMetaData();
+		//int columnsNumber = rsmd.getColumnCount();
+		while (myRs.next()) {
+			//for (int i = 1; i <= columnsNumber; i++) {
+				//if (i > 1) System.out.print(",  ");
+				//String columnValue = myRs.getString(i);
+				//System.out.print(columnValue + " " + rsmd.getColumnName(i));
+				Ship._name = myRs.getString("name");
+			}
+			//System.out.println("");
+		}
 
 
-public static void main (String[] args){
+
+	public static void main (String[] args){
 		loginUsername();
 		loginPassword();
 		createConnection();
 		statement("Select * from ships");
+		getShipInfo("'MotherRussia'");
 
 }
 
