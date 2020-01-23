0952633a0dcfdffe7c71f3474e999de7d251e2c1
Emil <emil.jonsson2@yh.nackademin.se>
2020-01-23 13:51:04 +0100

    latest jao

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,18 @@ public class Main {
     boolean exit;
 
     public static void main(String[] args) {
+        String namn = "Jonssoncargo";
+        Ship boat = new Ship(namn, 50, 5, 20, 666, 2, 2, "B", 100, true, "A", 1);
+        boat.start();
+        boat.printInfo();
         Main main = new Main();
         main.runMenu();
-
 
     }
 
     public void runMenu() {
         printHeader();
-        while(!exit) {
+        while (!exit) {
             printMenu();
             int choice = getInput();
             performAction(choice);
@@ -37,36 +40,37 @@ public class Main {
     private int getInput() {
         Scanner kb = new Scanner(System.in);
         int choice = -1;
-        while(choice < 0 || choice > 4) {
+        while (choice < 0 || choice > 4) {
             try {
                 System.out.print("\nEnter your choice: ");
                 choice = Integer.parseInt(kb.nextLine());
-            }
-            catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 System.out.println("Invalid selection. Please try again");
             }
         }
+        kb.close();
         return choice;
     }
 
     private void performAction(int choice) {
-        switch(choice) {
-            case 0:
-                exit = true;
-                System.out.println("Thank your for using our simulator");
-                break;
-            case 1:
-                //Ship();
-                break;
-            case 2:
-                //Order();
-                break;
-            case 3:
-                //Map();
-            case 4:
-                //Status();
-            default:
-                System.out.println("Unknown error has occured");
+        switch (choice) {
+        case 0:
+            exit = true;
+            System.out.println("Thank your for using our simulator");
+            break;
+        case 1:
+            // Ship();
+
+            break;
+        case 2:
+            // Order();
+            break;
+        case 3:
+            // Map();
+        case 4:
+            // Status();
+        default:
+            System.out.println("Unknown error has occured");
         }
     }
 }
